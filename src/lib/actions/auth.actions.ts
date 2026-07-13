e692b5e93852b50d4e173d4d98143abf0ca2ad55
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-07-13 07:16:48 +0600

    feat: setup auth server actions

---
+++ src/lib/actions/auth.actions.ts
@@ -0,0 +1,31 @@
+// src/lib/actions/auth.actions.ts
+"use server"
+
+import { loginSchema, registerSchema } from "@/utils/validation";
+import { z } from "zod";
+
+type LoginInput = z.infer<typeof loginSchema>;
+type RegisterInput = z.infer<typeof registerSchema>;
+
+// Auth actions
+export async function loginUser(data: LoginInput) {
+    const result = loginSchema.safeParse(data);
+
+    if (!result.success) {
+        return { success: false, message: "Invalid data", errors: result.error.format() };
+    }
+
+    console.log("Login Data:", result.data);
+    return { success: true, message: "Login Successful!" };
+}
+
+export async function registerUser(data: RegisterInput) {
+    const result = registerSchema.safeParse(data);
+
+    if (!result.success) {
+        return { success: false, message: "Invalid data", errors: result.error.format() };
+    }
+
+    console.log("Registration Data:", result.data);
+    return { success: true, message: "Registration Successful!" };
+}
